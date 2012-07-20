c588fa245228554ea1b591f941b5ff5576dd53d2
Nathan Dennis <nathan.dennis@monarchnc.org>
2012-07-20 08:43:54 -0400

    removing because repo is too large

--- target/testapp/resources/js/showMessage.js
+++
@@ -1,5 +0,0 @@
-function showMessage(message) {
-    jQuery('<div />', {class: 'rf-p-hdr'}).prependTo('#newMemberMessages').text(message).fadeOut(3000, function() {
-      $(this).remove();
-    });
-}
